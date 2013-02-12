9328e5992d0bdb2cfb6fa7744abd704f95678f85
Bob Grube <bobgrube3@gmail.com>
2013-02-11 19:14:43 -0500

    Initial Commit

---
+++ Budget/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Budget
+//
+//  Created by Bob Grube on 2/11/13.
+//  Copyright (c) 2013 Bob Grube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Budget/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Budget
+//
+//  Created by Bob Grube on 2/11/13.
+//  Copyright (c) 2013 Bob Grube. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
